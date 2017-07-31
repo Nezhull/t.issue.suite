3badc93831c7b64f500d8865f26d840684472d57
pavel.jakovlev <pavel.jakovlev@insoft.lt>
2017-07-31 17:13:20 +0300

    Implicit conversion and equals support for enum base.

--- T.Issue.Commons/Enum/EnumBase.cs
+++ T.Issue.Commons/Enum/EnumBase.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace T.Issue.Commons.Enum
 {
-    public abstract class EnumBase<T> where T : EnumBase<T>
+    public abstract class EnumBase<T> : IEquatable<EnumBase<T>> where T : EnumBase<T>
     {
         protected static List<T> EnumValues = new List<T>();
 
@@ -31,9 +32,64 @@ namespace T.Issue.Commons.Enum
             return null;
         }
 
+        public bool Equals(EnumBase<T> other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+            return Key == other.Key;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+            return Equals((EnumBase<T>) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Key;
+        }
+
         public override string ToString()
         {
             return Value;
+        }
+
+        public static implicit operator int(EnumBase<T> enm)
+        {
+            return enm.Key;
+        }
+
+        public static implicit operator EnumBase<T>(int key)
+        {
+            return GetBaseByKey(key);
+        }
+
+        public static implicit operator int?(EnumBase<T> enm)
+        {
+            return enm.Key;
+        }
+
+        public static implicit operator EnumBase<T>(int? key)
+        {
+            return key.HasValue ? GetBaseByKey(key.Value) : null;
         }
     }
 }
