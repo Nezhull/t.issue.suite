0b746611eccec497968eea19bbb4e51128806b06
pavel.jakovlev <pavel.jakovlev@insoft.lt>
2017-07-31 16:33:42 +0300

    Enum base object (for simulating Java enums).

---
+++ T.Issue.Commons/Enum/EnumBase.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace T.Issue.Commons.Enum
+{
+    public abstract class EnumBase<T> where T : EnumBase<T>
+    {
+        protected static List<T> EnumValues = new List<T>();
+
+        public virtual int Key { get; }
+        public virtual string Value { get; }
+
+        protected EnumBase(int key, string value)
+        {
+            Key = key;
+            Value = value;
+            EnumValues.Add((T) this);
+        }
+
+        protected static ReadOnlyCollection<T> GetBaseValues()
+        {
+            return EnumValues.AsReadOnly();
+        }
+
+        protected static T GetBaseByKey(int key)
+        {
+            foreach (T value in EnumValues)
+            {
+                if (value.Key == key) return value;
+            }
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
